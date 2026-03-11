56d95eee470e727523f9a9d27945fb6d8ea66dbc
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-03-11 11:19:31 +0100

    Gestion de la page de login

--- fotoblog/fotoblog/urls.py
+++ fotoblog/fotoblog/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from authentication import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.login_page, name='login'),
 ]
