f591c4f8f31424cadcce044d925dcb9034f651e3
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-03-11 15:37:45 +0100

    Gestion des vues basés sur des classes et gestion des vues génériques

--- fotoblog/fotoblog/urls.py
+++ fotoblog/fotoblog/urls.py
@@ -16,13 +16,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 
 import authentication.views
 import blog.views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', authentication.views.login_page, name='login'),
-    path('logout/', authentication.views.logout_user, name='logout'),
+    # path('login/', authentication.views.login_page, name='login'),
+    # path('login/', authentication.views.LoginPageView.as_view(), name='login'),
+
+    path('', LoginView.as_view(
+                template_name='authentication/login.html',
+                redirect_authenticated_user=True),
+                name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    path('change-password/', PasswordChangeView.as_view(
+        template_name='authentication/password_change_form.html'),
+         name='password_change'
+         ),
+    path('change-password-done/', PasswordChangeDoneView.as_view(
+        template_name='authentication/password_change_done.html'),
+         name='password_change_done'
+         ),
+    # path('logout/', authentication.views.logout_user, name='logout'),
     path('home/', blog.views.home, name='home'),
 ]
