00364e83b136446ea33c81a9dc1f8d618c2c06c4
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-03-12 17:10:49 +0100

    gestion formulaire modif et suppression billet blog sur la meme page + ajout de permissions pour ajouter des photos

--- fotoblog/blog/models.py
+++ fotoblog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from PIL import Image
 
 class Photo(models.Model):
     image = models.ImageField()
@@ -7,6 +8,18 @@ class Photo(models.Model):
     uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
 
+    IMAGE_MAX_SIZE = (800, 800)
+
+    def resize_image(self):
+        image = Image.open(self.image)
+        image.thumbnail(self.IMAGE_MAX_SIZE)
+        # sauvegarde de l’image redimensionnée dans le système de fichiers
+        # ce n’est pas la méthode save() du modèle !
+        image.save(self.image.path)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.resize_image()
 
 class Blog(models.Model):
     photo = models.ForeignKey(Photo, null=True, on_delete=models.SET_NULL, blank=True)
@@ -15,3 +28,13 @@ class Blog(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     starred = models.BooleanField(default=False)
+
+    word_count = models.IntegerField(null=True)
+
+    def _get_word_count(self):
+        words = self.content.split()
+        return len(words)
+
+    def save(self, *args, **kwargs):
+        self.word_count = self._get_word_count()
+        super().save(*args, **kwargs)
