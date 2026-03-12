e38d3a57ec90e405591be38e33e284dca7bcfafc
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-03-12 10:55:34 +0100

    Ajout fonctionnalités mise en ligne photo

--- fotoblog/fotoblog/urls.py
+++ fotoblog/fotoblog/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.conf import settings
+from django.conf.urls.static import static
 
 import authentication.views
 import blog.views
@@ -45,4 +47,12 @@ urlpatterns = [
          ),
     path('logout/', authentication.views.logout_user, name='logout'),
     path('home/', blog.views.home, name='home'),
+
+    path('photo/upload/', blog.views.photo_upload, name='photo_upload'),
+
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
