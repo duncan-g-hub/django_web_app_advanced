9278a597b62ea9fad591be746817f154fee0bb73
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-03-16 09:39:30 +0100

    Création des fluxs : blog + photos et photos uniquement

--- fotoblog/blog/views.py
+++ fotoblog/blog/views.py
@@ -1,7 +1,10 @@
+from itertools import chain
+
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import get_object_or_404
 from django.forms import formset_factory
+from django.db.models import Q
 
 from . import forms, models
 
@@ -85,11 +88,31 @@ def blog_and_photo_upload(request):
     return render(request, 'blog/create_blog_post.html', context=context)
 
 
+
+
 @login_required
-def home (request):
-    photos = models.Photo.objects.all()
-    blogs = models.Blog.objects.all()
-    return render(request, 'blog/home.html', context={'photos': photos, 'blogs': blogs})
+def home(request):
+    blogs = models.Blog.objects.filter(Q(author__in=request.user.follows) | Q(starred=True))
+    photos = models.Photo.objects.filter(uploader__in=request.user.follows).exclude(blog__in=blogs)
+    blogs_and_photos = sorted(
+        chain(blogs, photos),
+        key=lambda instance: instance.date_created,
+        reverse=True
+    )
+    context = {
+        'blogs_and_photos': blogs_and_photos,
+    }
+
+    return render(request, 'blog/home.html', context=context)
+
+
+@login_required
+def photo_feed(request):
+    photos = models.Photo.objects.filter(uploader__in=request.user.follows.all()).order_by('-date_created')
+    context = {
+        'photos': photos,
+    }
+    return render(request, 'blog/photo_feed.html', context=context)
 
 
 @login_required
